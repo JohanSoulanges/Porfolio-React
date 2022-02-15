4eed92e54bc101f0ad8da478648ed623352026e1
Vold69 <73841724+Vold69@users.noreply.github.com>
2022-02-15 12:17:29 +0100

    make it better

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import "./index.css";
 import "./Index.scss";
 import App from "./App";
 import { AppProvider } from "./Context/AppContext";
