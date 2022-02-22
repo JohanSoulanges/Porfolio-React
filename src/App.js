640f514ac27e720a06349c2217e4721cc21aed8e
Vold69 <73841724+Vold69@users.noreply.github.com>
2022-02-22 11:00:22 +0100

    "add meta test"

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import DocumentMeta from "react-document-meta";
 import Sidebar from "./Content/Sidebar";
 import Home from "./Content/Home";
 import Comp from "./Content/Comp";
@@ -8,9 +9,21 @@ import Question from "./Content/Question";
 import Contact from "./Content/Contact";
 
 function App() {
+  const meta = {
+    title: "Developpeur Full Stack",
+    description: "I am a porftolio made in React by johan Damioy",
+    canonical: "http://portfolio.com",
+    image: "../images/linkedin.jpg",
+    meta: {
+      charset: "utf-8",
+      name: {
+        keywords: "react,meta,document,html,tags",
+      },
+    },
+  };
   return (
     <>
-      <Home />
+      <Home {...meta} />
       <Comp />
       <Project />
       <Show />
