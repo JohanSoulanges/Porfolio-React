979ad9b0f1acb0cfa0dfedfd2bbcbf7392e12669
Vold69 <73841724+Vold69@users.noreply.github.com>
2022-02-22 11:11:47 +0100

    "add meta test 2"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import DocumentMeta from "react-document-meta";
 import Sidebar from "./Content/Sidebar";
 import Home from "./Content/Home";
 import Comp from "./Content/Comp";
@@ -9,21 +8,9 @@ import Question from "./Content/Question";
 import Contact from "./Content/Contact";
 
 function App() {
-  const meta = {
-    title: "Developpeur Full Stack",
-    description: "I am a porftolio made in React by johan Damioy",
-    canonical: "http://portfolio.com",
-    image: "../images/linkedin.jpg",
-    meta: {
-      charset: "utf-8",
-      name: {
-        keywords: "react,meta,document,html,tags",
-      },
-    },
-  };
   return (
     <>
-      <Home {...meta} />
+      <Home />
       <Comp />
       <Project />
       <Show />
